76487a88d808118bf2d389b8214dab14f4c948a1
wangqiaoling01 <wangqiaoling01@baijia.com>
2024-07-15 16:01:39 +0800

    feat: 搭建公共组件库

---
+++ examples/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Button }from '@common/components'
+import { add } from '@common/utils'
+import { onMounted } from 'vue';
+onMounted(() => {
+    console.log('mounted', add(1,2))
+})
+</script>
+
+<template>
+    <div class="example-wrapper">
+        <Button>222</Button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.example-wrapper {
+}
+</style>
